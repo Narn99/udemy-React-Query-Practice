b3fb5419d57961239eab12f1a7e0e0009305f6d7
Narn99 <vkdldjtmflr@naver.com>
2024-04-24 08:03:37 +0900

    실습 중

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,9 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import { fetchComments } from "./api";
 import "./PostDetail.css";
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  // const data = [];
+
+  const { data, isError, error, isLoading } = useQuery({
+    queryKey: ["comments", post.id], // post.id를 안 넣어주면, 쿼리 구분을 못 해서 업데이트 없이 캐시에 있는 것만 표시해줌
+    queryFn: () => fetchComments(post.id), // fetchComments(post.id)를 직접 넣으면 오류
+  });
+
+  if (isLoading) {
+    return <h3>Now Loading...</h3>;
+  }
+
+  if (isError) {
+    return <p>{error.toString()}</p>;
+  }
 
   return (
     <>
