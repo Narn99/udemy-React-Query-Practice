cf11645aea72c4198e40931f446221f0be64b0a3
Narn99 <vkdldjtmflr@naver.com>
2024-05-25 22:44:45 +0900

    낙관적 업데이트

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import jsonpatch from "fast-json-patch";
 
 import type { User } from "@shared/types";
@@ -7,6 +7,9 @@ import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 import { useUser } from "./useUser";
 
 import { toast } from "@/components/app/toast";
+import { queryKeys } from "@/react-query/constants";
+
+export const MUTATION_KEY = "patch-user";
 
 // for when we need a server function
 async function patchUserOnServer(
@@ -29,15 +32,23 @@ async function patchUserOnServer(
 }
 
 export function usePatchUser() {
-  const { user, updateUser } = useUser();
+  const { user } = useUser();
+  const queryClient = useQueryClient();
 
   // TODO: replace with mutate function
 
   const { mutate: patchUser } = useMutation({
+    mutationKey: [MUTATION_KEY],
     mutationFn: (newData: User) => patchUserOnServer(newData, user),
-    onSuccess: (userData: User | null) => {
-      updateUser(userData);
+    onSuccess: () => {
       toast({ title: "User Patch Success!", status: "success" });
+    },
+    onSettled: () => {
+      // onSuccess와 다르게 성공하든 오류가 뜨든 무조건 실행
+      // invalidate 이후 서버에서 데이터를 받기까지 변형이 진행중인 상태 유지를 위해 이 프로미스를 반환해야함
+      return queryClient.invalidateQueries({
+        queryKey: [queryKeys.user],
+      });
     },
   });
 
