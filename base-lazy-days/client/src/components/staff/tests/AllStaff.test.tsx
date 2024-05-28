fe33a99502a88ddec247a97037c77438b6a98823
Narn99 <vkdldjtmflr@naver.com>
2024-05-28 21:30:20 +0900

    testing code

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,7 +1,15 @@
 import { AllStaff } from "../AllStaff";
 
-// import { render, screen } from "@/test-utils";
+import { render, screen } from "@/test-utils";
 
-test("renders response from query", () => {
+test("renders response from query", async () => {
   // write test here
+
+  render(<AllStaff />);
+
+  const staffTitles = await screen.findAllByRole("heading", {
+    name: /sandra|divya|mateo|michael/i,
+  });
+
+  expect(staffTitles).toHaveLength(4);
 });
